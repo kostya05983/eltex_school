3e90b30fe9a82fa76e01d86472337680374ac8e2
kostya <kostya05983@gmail.com>
2018-04-01 20:59:54 +0700

    made in another way

--- bash/scriptOne.sh
+++ bash/scriptOne.sh
@@ -1,38 +1,35 @@
 #!/bin/bash
 read -p "write a location of directories " directory
 cd ./$directory
-read -p "write an amount of directories,you want to create " amount
+read -p "write an amount of directories,you want to create " amountDirectories
+read -p "write a name of directories " nameDirectory
+read -p "write an amount of subdirectories, you want to create " amountSubDirectories
+read -p "write a name of subdirectories " nameSubDirectories
+read -p "write an amount of files, you want to create in each subdirectory " amountFiles
+read -p "write a name of files " nameFile
 
-while [ $amount -gt 0 ]
+
+i=$amountSubDirectories
+j=$amountFiles
+while [ $amountDirectories -gt 0 ]
 do
-	read -p "write a name of directory " name
-	mkdir $name
-	read -p "do you want to create a subfolder y or n " answer
-		if [ $answer = "y" ]
-		then
-			cd ./$name
-			read -p "write an amount of directories,you want to create " amountSubFolder
-			while [ $amountSubFolder -gt 0 ]
-			do
-				read -p "write a name of subDirectory " name
-				mkdir $name
-				read -p "do you want to create a files in directory y or n " answer
-					if [ $answer = "y" ]
-					then
-						cd ./$name
-						read -p "write an amount of files,you want to create " amountFiles
-							while [ $amountFiles -gt 0 ]
-							do
-								read -p "write a name of file " name
-								>$name
-								amountFiles=$[ $amountFiles - 1 ]
-							done
-						cd ..
-					fi
-				amountSubFolder=$[ $amountSubFolder - 1 ]
-			done
-		cd ..	
-		fi
-	amount=$[ $amount - 1 ]
+mkdir $nameDirectory$amountDirectories
+cd ./$nameDirectory$amountDirectories
+while [ $i -gt 0 ]
+do
+mkdir $nameSubDirectories$i
+cd ./$nameSubDirectories$i
+while [ $j -gt 0 ]
+do
+>$nameFile$j.txt
+j=$[ $j - 1]
+done
+j=$amountFiles
+cd ..
+i=$[ $i - 1]
+done
+i=$amountSubDirectories
+cd ..
+amountDirectories=$[ $amountDirectories - 1]
 done
 exit 0
